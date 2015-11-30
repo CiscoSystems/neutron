71b7dc39781e69e25abe76ee606808a83d8563f4
Bob Melander <bob.melander@gmail.com>
2015-11-30 15:20:10 +0100

    Retry if HA group info is not found for HA enabled router
    This patch changes how HA information is added to ports for
    an HA enabled router. If the HA group for a router and a particular
    subnet is not found in the DB, the plugin will retry the DB
    query a few times in hope that the HA group DB entry will be
    commited to the DB during those retries.
    
    The patchs also makes a few minor code cleanups of the code
    handling the HA group info, in particular make use of a constant
    defined in the HA extension instead of its string value directly.
    
    Partially closes bug: DE936
    
    Change-Id: Ic5ff8b1b3d4ba97a5aec995930f47e775c3a030c

--- neutron/plugins/cisco/cfg_agent/device_drivers/asr1k/asr1k_cfg_syncer.py
+++ neutron/plugins/cisco/cfg_agent/device_drivers/asr1k/asr1k_cfg_syncer.py
@@ -18,6 +18,7 @@ import netaddr
 from neutron.common import constants
 from neutron.i18n import _LI
 from neutron.plugins.cisco.common import cisco_constants
+from neutron.plugins.cisco.extensions import ha
 from neutron.plugins.cisco.extensions import routerrole
 
 import re
@@ -31,6 +32,7 @@ from neutron.plugins.cisco.cfg_agent.device_drivers.asr1k import (
 from oslo_log import log as logging
 
 LOG = logging.getLogger(__name__)
+
 
 ROUTER_ROLE_ATTR = routerrole.ROUTER_ROLE_ATTR
 
@@ -479,7 +481,7 @@ class ConfigSyncer(object):
             gw_port = router['gw_port']
             #gw_net_id = gw_port['network_id']
             #gw_hsrp_num = self._get_hsrp_grp_num_from_net_id(gw_net_id)
-            gw_hsrp_num = int(gw_port['ha_info']['group'])
+            gw_hsrp_num = int(gw_port[ha.HA_INFO]['group'])
             gw_segment_id = gw_port['hosting_info']['segmentation_id']
             if segment_id != gw_segment_id:
                 LOG.info(_LI("snat segment_id does not match router's"
@@ -733,7 +735,7 @@ class ConfigSyncer(object):
 
     def subintf_hsrp_ip_check(self, intf_list, is_external, ip_addr):
         for target_intf in intf_list:
-            ha_intf = target_intf['ha_info']['ha_port']
+            ha_intf = target_intf[ha.HA_INFO]['ha_port']
             target_ip = ha_intf['fixed_ips'][0]['ip_address']
             LOG.info(_LI("target_ip: %(target_ip)s, actual_ip: %(ip_addr)s") %
                      {'target_ip': target_ip,
@@ -910,7 +912,7 @@ class ConfigSyncer(object):
 
             # self.existing_cfg_dict['interfaces'][intf.segment_id] = intf
 
-            correct_grp_num = int(db_intf['ha_info']['group'])
+            correct_grp_num = int(db_intf[ha.HA_INFO]['group'])
 
             if intf.has_ipv6 is False:
                 if self.clean_interfaces_nat_check(intf,
