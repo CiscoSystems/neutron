86750f91bb172433737e9d5a50970e54a05215c5
Nelson Huang <nelson.huang@onecloudinc.com>
2015-12-22 17:18:56 -0500

    Add runtime options for cfg_agent_debug module.  Updated UT.
    Default cfg_agent_debug configuration
    [cfg_agent]
    enable_cfg_agent_debug=False
    max_parent_records=200
    max_child_records=50

--- neutron/plugins/cisco/cfg_agent/cfg_agent_debug.py
+++ neutron/plugins/cisco/cfg_agent/cfg_agent_debug.py
@@ -12,13 +12,31 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import datetime
+from collections import OrderedDict
+from oslo_config import cfg
 
+import datetime
+import logging
 import pprint
 import prettytable
 
-MAX_RECORDS_PER_ROUTER = 100
-MAX_ROUTERS = 50
+LOG = logging.getLogger(__name__)
+
+# all-together, 10,000 records
+CFG_AGENT_DEBUG_OPTS = [
+    cfg.BoolOpt('enable_cfg_agent_debug',
+                default=False,
+                help=_("Enables/Disables cfg_agent debug module")),
+    cfg.IntOpt('max_parent_records',
+               default=200,
+               help=_("Max number of keys for each debug dictionary")),
+    cfg.IntOpt('max_child_records',
+               default=50,
+               help=_("Max number of child records"
+                      " for each debug dict record")),
+]
+
+cfg.CONF.register_opts(CFG_AGENT_DEBUG_OPTS, "cfg_agent")
 
 
 class CfgAgentDebug(object):
@@ -39,34 +57,74 @@ class CfgAgentDebug(object):
         #   req_id: <string>
         #   txn type: [router-intf-added]
         # }
-        self.routers = {}
-        self.total_router_txns = 0
+        self.routers = OrderedDict()
 
         # a lookup table of transactions/events pertaining to a hosting-device
         self.hosting_devices = {}
 
     def __repr__(self):
         ret_val = {'router_txns': self.routers,
-                   'total_router_txns': self.total_router_txns,
+                   'total_router_txns': self._get_total_txn_count(),
                    'hosting_device_txns': self.hosting_devices}
         return "%s" % pprint.pformat(ret_val)
+
+    @staticmethod
+    def _enforce_parent_record_constraints(debug_dict):
+        max_parent_records = cfg.CONF.cfg_agent.max_parent_records
+        if (debug_dict is not None and
+            len(debug_dict) >= max_parent_records):
+            # eject and log
+            key = debug_dict.keys().pop(0)
+            txns = debug_dict.pop(key)
+
+            return (key, txns)
+        else:
+            return (None, None)
+
+    @staticmethod
+    def _add_child_record(debug_dict, key, value):
+        """
+        This helper static method enforces the max child records
+        constraint when attempting to add a debug_dict child record
+        """
+        cfg.CONF.cfg_agent.max_child_records
+        if key not in debug_dict:
+            debug_dict[key] = []
+
+        child_records = debug_dict[key]
+
+        if (len(child_records) >= cfg.CONF.cfg_agent.max_child_records):
+            popped_record = child_records.pop(0)
+            LOG.debug("popped record = %s" % (pprint.pformat(popped_record)))
+            LOG.debug("len child records = %d" % (len(child_records)))
+
+        child_records.append(value)
 
     def add_request(self, request_id):
         self.requests[request_id] = {'time': datetime.datetime.strftime(
                        datetime.datetime.now(), format='%Y-%m-%d %H:%M:%S.%f')}
 
     def add_router_txn(self, router_id, txn_type, request_id=None):
+        if not cfg.CONF.cfg_agent.enable_cfg_agent_debug:
+            return
 
         if router_id not in self.routers:
+
+            popped_router_key, popped_router_txns = \
+                CfgAgentDebug._enforce_parent_record_constraints(self.routers)
+
+            if (popped_router_key is not None and
+                popped_router_txns is not None):
+                LOG.debug("Popped key %s, val = %s" % (popped_router_key,
+                                           pprint.pformat(popped_router_txns)))
             self.routers[router_id] = []
+
         txn_record = {'time': datetime.datetime.strftime(
                        datetime.datetime.now(), format='%Y-%m-%d %H:%M:%S.%f'),
                       'request_id': request_id,
                       'txn_type': txn_type}
 
-        self.routers[router_id].append(txn_record)
-
-        self.total_router_txns += 1
+        CfgAgentDebug._add_child_record(self.routers, router_id, txn_record)
 
     def get_router_txns_strfmt(self, router_id):
         """
--- neutron/tests/unit/plugins/cisco/cfg_agent/test_cfg_agent_debug.py
+++ neutron/tests/unit/plugins/cisco/cfg_agent/test_cfg_agent_debug.py
@@ -15,22 +15,31 @@
 
 from neutron.plugins.cisco.cfg_agent import (cfg_agent_debug)
 from neutron.tests import base
+from oslo_config import cfg
 
-import pprint
+# import pprint
 
 
 class CfgAgentDebug(base.BaseTestCase):
 
     def setUp(self):
         super(CfgAgentDebug, self).setUp()
+        cfg.CONF.set_override('enable_cfg_agent_debug', False, 'cfg_agent')
+        cfg.CONF.set_override('max_parent_records', 101, 'cfg_agent')
+        cfg.CONF.set_override('max_child_records', 1, 'cfg_agent')
         self.cfg_agent_debug = cfg_agent_debug.CfgAgentDebug()
 
     def tearDown(self):
         super(CfgAgentDebug, self).tearDown()
 
     def test_process_plugin_routers_data(self):
+        """
+        In this test, 101 parent records and 1 child record
+        are added to the routers debug dict
+        """
         router_id_spec = 'nrouter-abc%d-0000001'
         request_id_spec = 'req-abc%d'
+        cfg.CONF.set_override('enable_cfg_agent_debug', True, 'cfg_agent')
 
         for i in xrange(0, 101):
             router_id = router_id_spec % i
@@ -39,11 +48,46 @@ class CfgAgentDebug(base.BaseTestCase):
             self.cfg_agent_debug.add_router_txn(router_id,
                                                 'ADD_GW_PORT',
                                                 request_id)
-        self.assertEqual(101, self.cfg_agent_debug.total_router_txns)
-        self.assertEqual(self.cfg_agent_debug.total_router_txns,
+        self.assertEqual(101,
                          self.cfg_agent_debug._get_total_txn_count())
 
-        print(self.cfg_agent_debug.get_all_router_txns_strfmt())
-        print("Just nrouter-abc70-0000001 txns")
+        # print(self.cfg_agent_debug.get_all_router_txns_strfmt())
+        print("Just nrouter-abc100-0000001 txns")
+        # expected_txns = [{req_id: 'req-abc100', txn_type: 'ADD_GW_PORT'}]
         print(self.cfg_agent_debug.get_router_txns_strfmt(
-                                                    'nrouter-abc70-0000001'))
+                                                    'nrouter-abc100-0000001'))
+        # self.assertAlmostEquals(expected_txns,
+
+    def test_process_plugin_routers_data_constrained(self):
+        """
+        In this test, max parent records and child records are constrained.
+        """
+        router_id_spec = 'nrouter-abc%d-0000001'
+        request_id_spec = 'req-abc%d'
+
+        cfg.CONF.set_override('enable_cfg_agent_debug', True, 'cfg_agent')
+        cfg.CONF.set_override('max_parent_records', 2, 'cfg_agent')
+        cfg.CONF.set_override('max_child_records', 2, 'cfg_agent')
+
+        for i in xrange(0, 101):
+            router_id = router_id_spec % i
+            request_id = request_id_spec % i
+            self.cfg_agent_debug.add_router_txn(router_id,
+                                                'ADD_GW_PORT',
+                                                request_id)
+            self.cfg_agent_debug.add_router_txn(router_id,
+                                                'ADD_ROUTER_INTF',
+                                                request_id)
+
+            self.cfg_agent_debug.add_router_txn(router_id,
+                                                'REMOVE_GW_PORT',
+                                                request_id)
+        self.assertEqual(2, len(self.cfg_agent_debug.routers))
+        self.assertEqual(4,
+                         self.cfg_agent_debug._get_total_txn_count())
+
+        # print(self.cfg_agent_debug.get_all_router_txns_strfmt())
+        print("Just nrouter-abc100-0000001 txns")
+        # expected_txns = [{req_id: 'req-abc100', txn_type: 'ADD_GW_PORT'}]
+        print(self.cfg_agent_debug.get_router_txns_strfmt(
+                                                    'nrouter-abc100-0000001'))
