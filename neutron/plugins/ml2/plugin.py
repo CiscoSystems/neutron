CiscoSystems/neutron

commit 2ee08c3464c53abaf9bc5493132ad7958611e3b8

Move postcommit ops out of transaction for bulk
Currently, the bulk create operations in ML2 are executed in
a transaction. This means all precommit and postcommit operations
for such operations are in a transaction. Postcommit operations
are expected to be executed outside of transactions as they may
communicate with a backend and introduce substantial delays. This
fix removes the postcommit operations from the transaction for
bulk create network/subnet/port operations.

Change-Id: I9a9683058088e50d9443040223232bf5e1396ccf
Closes-Bug: #1193861

neutron/plugins/ml2/plugin.py
@@ -126,8 +126,6 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
         self.type_manager.initialize()
         self.extension_manager.initialize()
         self.mechanism_manager.initialize()
-        # bulk support depends on the underlying drivers
-        self.__native_bulk_support = self.mechanism_manager.native_bulk_support
 
         self._setup_rpc()
 
@@ -485,10 +483,59 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
                                   segment[api.SEGMENTATION_ID],
                                   segment[api.PHYSICAL_NETWORK])
 
-    # TODO(apech): Need to override bulk operations
+    def _delete_objects(self, context, resource, objects):
+        delete_op = getattr(self, 'delete_%s' % resource)
+        for obj in objects:
+            try:
+                delete_op(context, obj['result']['id'])
+            except KeyError:
+                LOG.exception(_LE("Could not find %s to delete."),
+                              resource)
+            except Exception:
+                LOG.exception(_LE("Could not delete %(res)s %(id)s."),
+                              {'res': resource,
+                               'id': obj['result']['id']})
 
-    def create_network(self, context, network):
-        net_data = network['network']
+    def _create_bulk_ml2(self, resource, context, request_items):
+        objects = []
+        collection = "%ss" % resource
+        items = request_items[collection]
+        try:
+            with context.session.begin(subtransactions=True):
+                obj_creator = getattr(self, '_create_%s_db' % resource)
+                for item in items:
+                    attrs = item[resource]
+                    result, mech_context = obj_creator(context, item)
+                    objects.append({'mech_context': mech_context,
+                                    'result': result,
+                                    'attributes': attrs})
+
+        except Exception:
+            with excutils.save_and_reraise_exception():
+                LOG.exception(_LE("An exception occurred while creating "
+                                  "the %(resource)s:%(item)s"),
+                              {'resource': resource, 'item': item})
+
+        try:
+            postcommit_op = getattr(self.mechanism_manager,
+                                    'create_%s_postcommit' % resource)
+            for obj in objects:
+                postcommit_op(obj['mech_context'])
+            return objects
+        except ml2_exc.MechanismDriverError:
+            with excutils.save_and_reraise_exception():
+                resource_ids = [res['result']['id'] for res in objects]
+                LOG.exception(_LE("mechanism_manager.create_%(res)s"
+                                  "_postcommit failed for %(res)s: "
+                                  "'%(failed_id)s'. Deleting "
+                                  "%(res)ss %(resource_ids)s"),
+                              {'res': resource,
+                               'failed_id': obj['result']['id'],
+                               'resource_ids': ', '.join(resource_ids)})
+                self._delete_objects(context, resource, objects)
+
+    def _create_network_db(self, context, network):
+        net_data = network[attributes.NETWORK]
         tenant_id = self._get_tenant_id_for_create(context, net_data)
         session = context.session
         with session.begin(subtransactions=True):
@@ -504,7 +551,10 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
             mech_context = driver_context.NetworkContext(self, context,
                                                          result)
             self.mechanism_manager.create_network_precommit(mech_context)
+        return result, mech_context
 
+    def create_network(self, context, network):
+        result, mech_context = self._create_network_db(context, network)
         try:
             self.mechanism_manager.create_network_postcommit(mech_context)
         except ml2_exc.MechanismDriverError:
@@ -512,7 +562,12 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
                 LOG.error(_LE("mechanism_manager.create_network_postcommit "
                               "failed, deleting network '%s'"), result['id'])
                 self.delete_network(context, result['id'])
+
         return result
+
+    def create_network_bulk(self, context, networks):
+        objects = self._create_bulk_ml2(attributes.NETWORK, context, networks)
+        return [obj['result'] for obj in objects]
 
     def update_network(self, context, id, network):
         provider._raise_if_updates_provider_attributes(network['network'])
@@ -661,7 +716,7 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
                           " failed"))
         self.notifier.network_delete(context, id)
 
-    def create_subnet(self, context, subnet):
+    def _create_subnet_db(self, context, subnet):
         session = context.session
         with session.begin(subtransactions=True):
             result = super(Ml2Plugin, self).create_subnet(context, subnet)
@@ -670,6 +725,10 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
             mech_context = driver_context.SubnetContext(self, context, result)
             self.mechanism_manager.create_subnet_precommit(mech_context)
 
+        return result, mech_context
+
+    def create_subnet(self, context, subnet):
+        result, mech_context = self._create_subnet_db(context, subnet)
         try:
             self.mechanism_manager.create_subnet_postcommit(mech_context)
         except ml2_exc.MechanismDriverError:
@@ -678,6 +737,10 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
                               "failed, deleting subnet '%s'"), result['id'])
                 self.delete_subnet(context, result['id'])
         return result
+
+    def create_subnet_bulk(self, context, subnets):
+        objects = self._create_bulk_ml2(attributes.SUBNET, context, subnets)
+        return [obj['result'] for obj in objects]
 
     def update_subnet(self, context, id, subnet):
         session = context.session
@@ -780,8 +843,8 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
             # the fact that an error occurred.
             LOG.error(_LE("mechanism_manager.delete_subnet_postcommit failed"))
 
-    def create_port(self, context, port):
-        attrs = port['port']
+    def _create_port_db(self, context, port):
+        attrs = port[attributes.PORT]
         attrs['status'] = const.PORT_STATUS_DOWN
 
         session = context.session
@@ -796,7 +859,7 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
             binding = db.add_port_binding(session, result['id'])
             mech_context = driver_context.PortContext(self, context, result,
                                                       network, binding)
-            new_host_port = self._get_host_port_if_changed(mech_context, attrs)
+
             self._process_port_binding(mech_context, attrs)
 
             result[addr_pair.ADDRESS_PAIRS] = (
@@ -807,7 +870,12 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
                                                       dhcp_opts)
             self.mechanism_manager.create_port_precommit(mech_context)
 
-        # Notification must be sent after the above transaction is complete
+        return result, mech_context
+
+    def create_port(self, context, port):
+        attrs = port['port']
+        result, mech_context = self._create_port_db(context, port)
+        new_host_port = self._get_host_port_if_changed(mech_context, attrs)
         self._notify_l3_agent_new_port(context, new_host_port)
 
         try:
@@ -830,6 +898,35 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
                               "failed, deleting port '%s'"), result['id'])
                 self.delete_port(context, result['id'])
         return bound_context._port
+
+    def create_port_bulk(self, context, ports):
+        objects = self._create_bulk_ml2(attributes.PORT, context, ports)
+
+        for obj in objects:
+            # REVISIT(rkukura): Is there any point in calling this before
+            # a binding has been successfully established?
+            # TODO(banix): Use a single notification for all objects
+            self.notify_security_groups_member_updated(context,
+                                                       obj['result'])
+
+            attrs = obj['attributes']
+            if attrs and attrs.get(portbindings.HOST_ID):
+                new_host_port = self._get_host_port_if_changed(
+                    obj['mech_context'], attrs)
+                self._notify_l3_agent_new_port(context, new_host_port)
+
+        try:
+            for obj in objects:
+                obj['bound_context'] = self._bind_port_if_needed(
+                    obj['mech_context'])
+            return [obj['bound_context']._port for obj in objects]
+        except ml2_exc.MechanismDriverError:
+            with excutils.save_and_reraise_exception():
+                resource_ids = [res['result']['id'] for res in objects]
+                LOG.error(_LE("_bind_port_if_needed failed. "
+                              "Deleting all ports from create bulk '%s'"),
+                          resource_ids)
+                self._delete_objects(context, 'port', objects)
 
     def update_port(self, context, id, port):
         attrs = port['port']

neutron/plugins/ml2/drivers/mech_bigswitch/driver.py
@@ -55,8 +55,6 @@ class BigSwitchMechanismDriver(plugin.NeutronRestProxyV2Base,
         # register plugin config opts
         pl_config.register_config()
         self.evpool = eventlet.GreenPool(cfg.CONF.RESTPROXY.thread_pool_size)
-        # backend doesn't support bulk operations yet
-        self.native_bulk_support = False
 
         # init network ctrl connections
         self.servers = servermanager.ServerPool()

neutron/plugins/ml2/managers.py
@@ -267,13 +267,9 @@ class MechanismManager(stevedore.named.NamedExtensionManager):
                  [driver.name for driver in self.ordered_mech_drivers])
 
     def initialize(self):
-        # For ML2 to support bulk operations, each driver must support them
-        self.native_bulk_support = True
         for driver in self.ordered_mech_drivers:
             LOG.info(_LI("Initializing mechanism driver '%s'"), driver.name)
             driver.obj.initialize()
-            self.native_bulk_support &= getattr(driver.obj,
-                                                'native_bulk_support', True)
 
     def _call_on_drivers(self, method_name, context,
                          continue_on_failure=False):

neutron/tests/unit/ml2/drivers/cisco/nexus/test_cisco_mech.py
@@ -309,7 +309,7 @@ class TestCiscoPortsV2(CiscoML2MechanismTestCase,
             plugin_obj = manager.NeutronManager.get_plugin()
             orig = plugin_obj.create_port
             with mock.patch.object(plugin_obj,
-                                   'create_port') as patched_plugin:
+                                   '_create_port_db') as patched_plugin:
 
                 def side_effect(*args, **kwargs):
                     return self._fail_second_call(patched_plugin, orig,
@@ -343,7 +343,7 @@ class TestCiscoPortsV2(CiscoML2MechanismTestCase,
             plugin_obj = manager.NeutronManager.get_plugin()
             orig = plugin_obj.create_port
             with mock.patch.object(plugin_obj,
-                                   'create_port') as patched_plugin:
+                                   '_create_port_db') as patched_plugin:
 
                 def side_effect(*args, **kwargs):
                     return self._fail_second_call(patched_plugin, orig,
@@ -718,7 +718,7 @@ class TestCiscoNetworksV2(CiscoML2MechanismTestCase,
         with mock.patch('__builtin__.hasattr',
                         new=fakehasattr):
             with mock.patch.object(plugin_obj,
-                                   'create_network') as patched_plugin:
+                                   '_create_network_db') as patched_plugin:
                 def side_effect(*args, **kwargs):
                     return self._fail_second_call(patched_plugin, orig,
                                                   *args, **kwargs)
@@ -737,7 +737,7 @@ class TestCiscoNetworksV2(CiscoML2MechanismTestCase,
         plugin_obj = manager.NeutronManager.get_plugin()
         orig = plugin_obj.create_network
         with mock.patch.object(plugin_obj,
-                               'create_network') as patched_plugin:
+                               '_create_network_db') as patched_plugin:
 
             def side_effect(*args, **kwargs):
                 return self._fail_second_call(patched_plugin, orig,
@@ -769,7 +769,7 @@ class TestCiscoSubnetsV2(CiscoML2MechanismTestCase,
             plugin_obj = manager.NeutronManager.get_plugin()
             orig = plugin_obj.create_subnet
             with mock.patch.object(plugin_obj,
-                                   'create_subnet') as patched_plugin:
+                                   '_create_subnet_db') as patched_plugin:
 
                 def side_effect(*args, **kwargs):
                     self._fail_second_call(patched_plugin, orig,
@@ -792,7 +792,7 @@ class TestCiscoSubnetsV2(CiscoML2MechanismTestCase,
         plugin_obj = manager.NeutronManager.get_plugin()
         orig = plugin_obj.create_subnet
         with mock.patch.object(plugin_obj,
-                               'create_subnet') as patched_plugin:
+                               '_create_subnet_db') as patched_plugin:
             def side_effect(*args, **kwargs):
                 return self._fail_second_call(patched_plugin, orig,
                                               *args, **kwargs)

neutron/tests/unit/ml2/drivers/mechanism_bulkless.py
@@ -1,23 +1,0 @@
-# Copyright (c) 2014 OpenStack Foundation
-# All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-
-from neutron.plugins.ml2 import driver_api as api
-
-
-class BulklessMechanismDriver(api.MechanismDriver):
-    """Test mechanism driver for testing bulk emulation."""
-
-    def initialize(self):
-        self.native_bulk_support = False

neutron/tests/unit/ml2/test_ml2_plugin.py
@@ -90,14 +90,6 @@ class Ml2PluginV2TestCase(test_plugin.NeutronDbPluginV2TestCase):
         self.context = context.get_admin_context()
 
 
-class TestMl2BulkToggleWithBulkless(Ml2PluginV2TestCase):
-
-    _mechanism_drivers = ['logger', 'test', 'bulkless']
-
-    def test_bulk_disable_with_bulkless_driver(self):
-        self.assertTrue(self._skip_native_bulk)
-
-
 class TestMl2BulkToggleWithoutBulkless(Ml2PluginV2TestCase):
 
     _mechanism_drivers = ['logger', 'test']
@@ -165,6 +157,23 @@ class TestMl2PortsV2(test_plugin.TestPortsV2, Ml2PluginV2TestCase):
         self._delete('networks', n['network']['id'])
         flips = l3plugin.get_floatingips(context.get_admin_context())
         self.assertFalse(flips)
+
+    def test_create_ports_bulk_port_binding_failure(self):
+        ctx = context.get_admin_context()
+        with self.network() as net:
+            plugin = manager.NeutronManager.get_plugin()
+
+            with mock.patch.object(plugin, '_bind_port_if_needed',
+                side_effect=ml2_exc.MechanismDriverError(
+                    method='create_port_bulk')) as _bind_port_if_needed:
+
+                res = self._create_port_bulk(self.fmt, 2, net['network']['id'],
+                                             'test', True, context=ctx)
+
+                self.assertTrue(_bind_port_if_needed.called)
+                # We expect a 500 as we injected a fault in the plugin
+                self._validate_behavior_on_bulk_failure(
+                    res, 'ports', webob.exc.HTTPServerError.code)
 
     def test_delete_port_no_notify_in_disassociate_floatingips(self):
         ctx = context.get_admin_context()

commit d7f3c9ab7940a937151f812fe7f42dc22e88f949

BACKPORT From Mitaka, fixed merge confilcts. Notify about port create/update unconditionally
The notification about port create/update should be done no matter
if host (or any other field) was changed or not - it should be up to
handler to decide how to handle it.

This also fixes the bug when there was actually no notification to l3
dvr agent on compute node on new port creation:
_get_host_port_if_changed() always returned None - this is a regression
from commit 2ee08c3464c53abaf9bc5493132ad7958611e3b8

Closes-Bug: #1521524
Change-Id: I5bb416d2aaab632526f06a5620c0a4ea96340d21

Conflicts:
	neutron/plugins/ml2/plugin.py
	neutron/tests/unit/plugins/ml2/test_plugin.py

neutron/plugins/ml2/plugin.py
@@ -175,12 +175,6 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
                 if self.type_manager.network_matches_filters(network, filters)
                 ]
 
-    def _get_host_port_if_changed(self, mech_context, attrs):
-        binding = mech_context._binding
-        if attrs and portbindings.HOST_ID in attrs:
-            if binding.host != attrs.get(portbindings.HOST_ID):
-                return mech_context.current
-
     def _check_mac_update_allowed(self, orig_port, port, binding):
         unplugged_types = (portbindings.VIF_TYPE_BINDING_FAILED,
                            portbindings.VIF_TYPE_UNBOUND)
@@ -974,11 +968,9 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
     @oslo_db_api.wrap_db_retry(max_retries=db_api.MAX_RETRIES,
                                retry_on_request=True)
     def create_port(self, context, port):
-        attrs = port[attributes.PORT]
         result, mech_context = self._create_port_db(context, port)
-        new_host_port = self._get_host_port_if_changed(mech_context, attrs)
         # notify any plugin that is interested in port create events
-        kwargs = {'context': context, 'port': new_host_port}
+        kwargs = {'context': context, 'port': result}
         registry.notify(resources.PORT, events.AFTER_CREATE, self, **kwargs)
 
         try:
@@ -1013,9 +1005,7 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
         for obj in objects:
             attrs = obj['attributes']
             if attrs and attrs.get(portbindings.HOST_ID):
-                new_host_port = self._get_host_port_if_changed(
-                    obj['mech_context'], attrs)
-                kwargs = {'context': context, 'port': new_host_port}
+                kwargs = {'context': context, 'port': obj['result']}
                 registry.notify(
                     resources.PORT, events.AFTER_CREATE, self, **kwargs)
 
@@ -1117,7 +1107,6 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
             mech_context = driver_context.PortContext(
                 self, context, updated_port, network, binding, levels,
                 original_port=original_port)
-            new_host_port = self._get_host_port_if_changed(mech_context, attrs)
             need_port_update_notify |= self._process_port_binding(
                 mech_context, attrs)
             self.mechanism_manager.update_port_precommit(mech_context)
@@ -1125,7 +1114,7 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
         # Notifications must be sent after the above transaction is complete
         kwargs = {
             'context': context,
-            'port': new_host_port,
+            'port': updated_port,
             'mac_address_updated': mac_address_updated,
             'original_port': original_port,
         }

neutron/tests/unit/plugins/ml2/test_plugin.py
@@ -1346,78 +1346,18 @@ class TestMl2PluginCreateUpdateDeletePort(base.BaseTestCase):
         exit = transaction.__exit__.call_count
         self.assertEqual(enter, exit)
 
-    def _create_plugin_for_create_update_port(self, new_host_port):
+    def _create_plugin_for_create_update_port(self):
         plugin = ml2_plugin.Ml2Plugin()
         plugin.extension_manager = mock.Mock()
         plugin.type_manager = mock.Mock()
         plugin.mechanism_manager = mock.Mock()
         plugin.notifier = mock.Mock()
-        plugin._get_host_port_if_changed = mock.Mock(
-            return_value=new_host_port)
         plugin._check_mac_update_allowed = mock.Mock(return_value=True)
 
         self.notify.side_effect = (
             lambda r, e, t, **kwargs: self._ensure_transaction_is_closed())
 
         return plugin
-
-    def _test__get_host_port_if_changed(
-        self, mech_context, attrs=None, expected_retval=None):
-        with mock.patch.object(ml2_plugin.Ml2Plugin,
-                               '__init__',
-                               return_value=None):
-            plugin = ml2_plugin.Ml2Plugin()
-            test_return = plugin._get_host_port_if_changed(mech_context, attrs)
-            self.assertEqual(expected_retval, test_return)
-
-    def test__get_host_port_if_changed_no_attrs(self):
-        mech_context = mock.Mock()
-        mech_context._binding.host = 'Host-1'
-        self._test__get_host_port_if_changed(
-            mech_context, attrs=None, expected_retval=None)
-
-    def test__get_host_port_if_changed_no_binding_change(self):
-        mech_context = mock.Mock()
-        mech_context._binding.host = 'Host-1'
-        mech_context.current = {
-            'id': 'fake-id',
-            'mac_address': '2a:2b:2c:2d:2e:2f'
-        }
-        attrs = {'mac_address': '0a:0b:0c:0d:0e:0f'}
-        self._test__get_host_port_if_changed(
-            mech_context, attrs=attrs, expected_retval=None)
-
-        attrs = {
-            portbindings.HOST_ID: 'Host-1',
-            'mac_address': '0a:0b:0c:0d:0e:0f',
-        }
-        self._test__get_host_port_if_changed(
-            mech_context, attrs=attrs, expected_retval=None)
-
-    def test__get_host_port_if_changed_with_binding_removed(self):
-        expected_return = {
-            'id': 'fake-id',
-            portbindings.HOST_ID: None,
-            'mac_address': '2a:2b:2c:2d:2e:2f'
-        }
-        mech_context = mock.Mock()
-        mech_context._binding.host = 'Host-1'
-        mech_context.current = expected_return
-        attrs = {portbindings.HOST_ID: None}
-        self._test__get_host_port_if_changed(
-            mech_context, attrs=attrs, expected_retval=expected_return)
-
-    def test__get_host_port_if_changed_with_binding_added(self):
-        expected_return = {
-            'id': 'fake-id',
-            portbindings.HOST_ID: 'host-1',
-            'mac_address': '2a:2b:2c:2d:2e:2f'
-        }
-        mech_context = mock.Mock()
-        mech_context.current = expected_return
-        attrs = {portbindings.HOST_ID: 'host-1'}
-        self._test__get_host_port_if_changed(
-            mech_context, attrs=attrs, expected_retval=expected_return)
 
     def test_create_port_rpc_outside_transaction(self):
         with contextlib.nested(
@@ -1426,14 +1366,15 @@ class TestMl2PluginCreateUpdateDeletePort(base.BaseTestCase):
         ) as (init, super_create_port):
             init.return_value = None
 
-            new_host_port = mock.Mock()
-            plugin = self._create_plugin_for_create_update_port(new_host_port)
+            new_port = mock.MagicMock()
+            super_create_port.return_value = new_port
+            plugin = self._create_plugin_for_create_update_port()
 
             plugin.create_port(self.context, mock.MagicMock())
 
-            kwargs = {'context': self.context, 'port': new_host_port}
+            kwargs = {'context': self.context, 'port': new_port}
             self.notify.assert_called_once_with('port', 'after_create',
-                plugin, **kwargs)
+                                                plugin, **kwargs)
 
     def test_update_port_rpc_outside_transaction(self):
         port_id = 'fake_id'
@@ -1460,22 +1401,23 @@ class TestMl2PluginCreateUpdateDeletePort(base.BaseTestCase):
                 mock.patch.object(ml2_db, 'get_locked_port_and_binding',
                                   return_value=(original_port_db, binding)),\
                 mock.patch.object(base_plugin.NeutronDbPluginV2,
-                                  'update_port'):
+                                  'update_port') as db_update_port:
             init.return_value = None
-            new_host_port = mock.Mock()
-            plugin = self._create_plugin_for_create_update_port(new_host_port)
+            updated_port = mock.MagicMock()
+            db_update_port.return_value = updated_port
+            plugin = self._create_plugin_for_create_update_port()
             original_port = plugin._make_port_dict(original_port_db)
 
             plugin.update_port(self.context, port_id, mock.MagicMock())
 
             kwargs = {
                 'context': self.context,
-                'port': new_host_port,
+                'port': updated_port,
                 'mac_address_updated': True,
                 'original_port': original_port,
             }
             self.notify.assert_called_once_with('port', 'after_update',
-                plugin, **kwargs)
+                                                plugin, **kwargs)
 
     def test_notify_outside_of_delete_transaction(self):
         self.notify.side_effect = (
@@ -1492,7 +1434,7 @@ class TestMl2PluginCreateUpdateDeletePort(base.BaseTestCase):
                               'get_service_plugins',
                               return_value={'L3_ROUTER_NAT': l3plugin}),
         ):
-            plugin = self._create_plugin_for_create_update_port(mock.Mock())
+            plugin = self._create_plugin_for_create_update_port()
             # deleting the port will call registry.notify, which will
             # run the transaction balancing function defined in this test
             plugin.delete_port(self.context, 'fake_id')